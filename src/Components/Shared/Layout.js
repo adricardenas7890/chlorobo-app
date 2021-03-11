16bb41dea2b58e8d5fed37d31437749c4c82b879
Adriana Cardenas <adricardenas7890@hotmail.com>
2021-03-10 22:39:31 -0600

    Added Layout.js

---
+++ src/Components/Shared/Layout.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Header from './Header';
+import Puzzle from '../Puzzle/';
+
+// This will contain the header (which may be hidden for introduction) and the body of the page
+class Layout extends React.Component {
+    constructor(props) {
+
+    }
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+
+}
