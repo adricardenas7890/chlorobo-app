d58e2249cc557f05fa85de9954a8709a9fe8f049
Adriana Cardenas <adricardenas7890@hotmail.com>
2021-03-10 22:39:15 -0600

    Added Puzzle.js

---
+++ src/Components/Puzzle/Puzzle.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+// This class will render puzzle components 
+
+class Puzzle extends React.Component {
+    constructor(props) {
+
+    }
+  
+    render() {
+        return (
+            <div className="puzzle-container">
+                <div>
+                    {/* INSERT PUZZLE LOGIC HERE */}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Puzzle
