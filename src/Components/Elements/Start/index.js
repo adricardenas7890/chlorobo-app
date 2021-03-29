6a9756eef2a8f6120d13e22596fd3ffa611c1b43
Benjamin Randall <bengr35@gmail.com>
2021-03-28 23:22:50 -0500

    Configure mobile detection and splash screen

--- src/Components/Elements/Start/index.js
+++ src/Components/Elements/Start/index.js
@@ -3,20 +3,36 @@ import { Button } from 'react-bootstrap';
 import {mainMenuPage} from '../../Pages/Content/contentSlice';
 import { useDispatch } from 'react-redux';
 import splashImage from './splash.png';
+import { isMobile, isChrome } from 'react-device-detect';
+import "./index.css"
 
 const Start = () => {
         const dispatch = useDispatch();
-        return (
-            <div>
+        if (isMobile) {
+            return (
                 <div>
-                    <img src={splashImage} className="App-logo" alt="Chlorobo-title" />
+                    <div>
+                        <img src={splashImage} className="mobile-splash" alt="Chlorobo-title" />
+                    </div>
+                    <div>
+                        <h1 className="mobile-text-1">This site is not accessible on mobile.</h1>
+                        <h1 className="mobile-text-2">Please re-login from a desktop device.</h1>
+                    </div>
                 </div>
-                <br/>
+            )
+        } else {
+            return (
                 <div>
-                	<Button variant="light" onClick={() => dispatch(mainMenuPage())} >ENTER</Button>
+                    <div>
+                        <img src={splashImage} className="App-logo" alt="Chlorobo-title" />
+                    </div>
+                    <br/>
+                    <div>
+                        <Button variant="light" onClick={() => dispatch(mainMenuPage())} >ENTER</Button>
+                    </div>
                 </div>
-            </div>
-        ) 
+            ) 
+        }
 }
 
 export default Start
