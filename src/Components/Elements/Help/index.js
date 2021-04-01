323fc8e05343f563106f0fc12c19e0ccb53f6ff4
Adriana Cardenas <adricardenas7890@hotmail.com>
2021-03-31 22:21:28 -0500

    changed class to className

--- src/Components/Elements/Help/index.js
+++ src/Components/Elements/Help/index.js
@@ -4,25 +4,25 @@ import './index.css';
 const Help = () => {
     return (
         <div>
-        	<div class="help-hover" id="Puzzle1">
+        	<div className="help-hover" id="Puzzle1">
             	<p>Chapter 1</p>
             </div>
-            <div class="help-hover" id="Puzzle2">
+            <div className="help-hover" id="Puzzle2">
             	<p>Chapter 2</p>
             </div>
-            <div class="help-hover" id="Puzzle3">
+            <div className="help-hover" id="Puzzle3">
             	<p>Chapter 3</p>
             </div>
-            <div class="help-hover" id="Puzzle4">
+            <div className="help-hover" id="Puzzle4">
             	<p>Chapter 4</p>
             </div>
-            <div class="help-hover" id="Puzzle5">
+            <div className="help-hover" id="Puzzle5">
             	<p>Chapter 5</p>
             </div>
-            <div class="help-hover" id="Puzzle6">
+            <div className="help-hover" id="Puzzle6">
             	<p>Chapter 6</p>
             </div>
-            <div class="help-hover" id="Puzzle7">
+            <div className="help-hover" id="Puzzle7">
             	<p>Chapter 7</p>
             </div>
         </div>
