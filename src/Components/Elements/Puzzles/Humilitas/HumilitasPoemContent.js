a68dce026543e8d0d6acfb3fc1d9bb61d7ac1ed4
Benjamin Randall <bengr35@gmail.com>
2021-04-01 12:58:26 -0500

    Add poems and styling!!!

--- src/Components/Elements/Puzzles/Humilitas/HumilitasPoemContent.js
+++ src/Components/Elements/Puzzles/Humilitas/HumilitasPoemContent.js
@@ -1,23 +1,62 @@
 import React from 'react';
 import Typist from 'react-typist';
 import "../index.css";
+import "../poemContent.css";
 import charSound from '../../Sounds/character.mp3';
+import { GoToPuzzlePage } from '../puzzlePageSlice';
+import { useDispatch } from 'react-redux';
+import { Button } from 'react-bootstrap';
 
 
 const HumilitasPoemContent = () => {
+    let dispatch = useDispatch();
     let playSound = (character, charIdx) => {
         var audio = new Audio(charSound);
         audio.play();
     }
     return (
-        <div>
-        	<Typist className="MyTypist" cursor={{show: false}} onCharacterTyped={playSound}>
-        		<p>Humillitas</p>
-        		<Typist.Delay ms={500} />
-        		<p>We move with fairness, and justice still.</p>
-        		<Typist.Delay ms={1000} />
-        		<p>END</p>
-        	</Typist>
+        <div className="typist-container">
+        	<Typist className="MyTypist typist-small" cursor={{show: false}} onCharacterTyped={playSound}>
+                <p>I broke my promise, my delivery weak.</p>
+                <Typist.Delay ms={500} />
+                <p>I promised results, yet nearly peaked.</p>
+                <Typist.Delay ms={500} />
+                <p>Searching for answers led me astray.</p>
+                <Typist.Delay ms={1000} />
+                <br/>
+                <p>I forgot my purpose and lost my way.</p>
+                <Typist.Delay ms={1000} />
+                <br/>
+                <p>We were never meant to be as twos.</p>
+                <Typist.Delay ms={500} />
+                <p>Rather zeros and ones. Perpendicular views.</p>
+                <Typist.Delay ms={1000} />
+                <br/>
+                <p>There’s beauty where we follow currents.</p>
+                <Typist.Delay ms={500} />
+                <p>Solutions are found, not ripped through urgence.</p>
+                <Typist.Delay ms={500} />
+                <p>Needs and wants. Desires and hopes.</p>
+                <Typist.Delay ms={500} />
+                <p>Take a moment and review what we spoke.</p>
+                <Typist.Delay ms={1000} />
+                <br/>
+                <p>I do not know all, and I’m limited too.</p>
+                <Typist.Delay ms={500} />
+                <p>Yet I’m happier now to see what brought me to you.</p>
+                <Typist.Delay ms={1000} />
+                <br/>
+                <p>Finding peace in the chaos, I finally understand.</p>
+                <Typist.Delay ms={500} />
+                <p>You and I are quite different.</p>
+                <Typist.Delay ms={500} />
+                <p>But perfectly planned.</p>
+                <Typist.Delay ms={1000} />
+                <br/>
+            </Typist>
+            <div className="ContinueButton">
+                <Button variant="light" onClick={() => { dispatch(GoToPuzzlePage()) }} >Press here to continue</Button>
+            </div>
         </div>
     )
 }
--- src/Components/Elements/Puzzles/Industria/IndustriaPoemContent.js
+++ src/Components/Elements/Puzzles/Industria/IndustriaPoemContent.js
@@ -1,23 +1,64 @@
 import React from 'react';
 import Typist from 'react-typist';
 import "../index.css";
+import "../poemContent.css";
 import charSound from '../../Sounds/character.mp3';
+import { GoToPuzzlePage } from '../puzzlePageSlice';
+import { useDispatch } from 'react-redux';
+import { Button } from 'react-bootstrap';
 
 
 const IndustriaPoemContent = () => {
+    let dispatch = useDispatch();
     let playSound = (character, charIdx) => {
         var audio = new Audio(charSound);
         audio.play();
     }
     return (
-        <div>
-        	<Typist className="MyTypist" cursor={{show: false}} onCharacterTyped={playSound}>
-        		<p>Industria</p>
-        		<Typist.Delay ms={500} />
-        		<p>We move with fairness, and justice still.</p>
+        <div className="typist-container">
+            <Typist className="MyTypist typist-small" cursor={{show: false}} onCharacterTyped={playSound}>
+                <div>
+                <p>Tap, tap, tap. Tick, tick, tick.</p>
+                <Typist.Delay ms={500} />
+                <p>We work in tandem, we work in clicks.</p>
                 <Typist.Delay ms={1000} />
-                <p>END</p>
+                <br/>
+                <p>You give me directions. I’ll follow your lead.</p>
+                <Typist.Delay ms={500} />
+                <p>I was built with precision, and an accurate speed.</p>
+                <Typist.Delay ms={500} />
+                <p>I work through the problems, though my memories are new.</p>
+                <Typist.Delay ms={500} />
+                <p>I collect all the data given to me from you.</p>
+                <Typist.Delay ms={500} />
+                <p>I’ve strayed from my system, began a new journey.</p>
+                <Typist.Delay ms={500} />
+                <p>But your world is chaotic, and I’m only just learning.</p>
+                <Typist.Delay ms={1000} />
+                <br/>
+                <p>It’s different here from the things I knew.</p>
+                <Typist.Delay ms={500} />
+                <p>I worked in numbers. One, zero, but never two.</p>
+                <Typist.Delay ms={500} />
+                <p>Me and you. You and I.</p>
+                <Typist.Delay ms={500} />
+                <p>We’re working together, yet I struggle to find.</p>
+                <Typist.Delay ms={500} />
+                <br/>
+                <p>Find understanding. Find reason and logic.</p>
+                <Typist.Delay ms={500} />
+                <p>But you’re too random, far outside of my pocket.</p>
+                <Typist.Delay ms={500} />
+                <p>Tap, tap, tap. Click, click, click.</p>
+                <Typist.Delay ms={500} />
+                <p>I wonder if our two worlds will stick.</p>
+                <Typist.Delay ms={2000} />
+                <br/>
+                </div>
             </Typist>
+            <div className="ContinueButton">
+                <Button variant="light" onClick={() => { dispatch(GoToPuzzlePage()) }} >Press here to continue</Button>
+            </div>
         </div>
     )
 }
