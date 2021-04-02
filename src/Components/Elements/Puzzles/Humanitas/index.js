a76a21a3caa437ed48a94308225c25cc2b14f69c
Adriana Cardenas <adricardenas7890@hotmail.com>
2021-04-01 19:36:13 -0500

    added the input field to humanitas

--- src/Components/Elements/Puzzles/Humanitas/index.js
+++ src/Components/Elements/Puzzles/Humanitas/index.js
@@ -2,18 +2,25 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 import { SetSolved } from '../puzzleProgressSlice';
 import { useDispatch } from 'react-redux';
-import '../index.css';
+import './index.css';
 
 const Humanitas = () => {
-    let title = "Humanitas";
     let dispatch = useDispatch();
     return (
         // <div className="castitas-container">
         //     This is the container for Humanitas.
         // </div>
         <div className="main-content-holder">
-            <div className="ingame-puzzle-name-div"> <div className="ingame-puzzle-name">{title}</div></div>
+            <div className="ingame-puzzle-name-div"> <div className="ingame-puzzle-name">&nbsp;</div></div>
             <div className="main-puzzle-holder">
+                <div className="humanitas-prompt-div">
+                    <p> Here is the prompt</p>
+                </div>
+                <div className="humanitas-input-container">
+                    <div className="humanitas-input-div">
+                        <input className="humanitas-input" type="text"/>
+                    </div>
+                </div>
                 <Button variant="light" id="solvePuzzleButton" onClick={() => { dispatch(SetSolved(7));}}> click to solve this puzzle</Button>
                 
             
