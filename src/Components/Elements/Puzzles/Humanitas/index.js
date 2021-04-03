02b89b389b9ce125ba0d64ac5e046beb4e553759
Adriana Cardenas <adricardenas7890@hotmail.com>
2021-04-03 00:29:08 -0500

    working humanitas, no CSS

--- src/Components/Elements/Puzzles/Humanitas/index.js
+++ src/Components/Elements/Puzzles/Humanitas/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {setState} from 'react';
 import { Button } from 'react-bootstrap';
 import { SetSolved } from '../puzzleProgressSlice';
 import { useDispatch } from 'react-redux';
@@ -6,6 +6,14 @@ import './index.css';
 
 const Humanitas = () => {
     let dispatch = useDispatch();
+
+    let handleSendResponse = (input) => { 
+        // send it somewhere here
+        dispatch(SetSolved(7));
+        console.log(input);
+        alert(input);
+        alert("you solved the puzzle");
+    }
     return (
         // <div className="castitas-container">
         //     This is the container for Humanitas.
@@ -16,17 +24,58 @@ const Humanitas = () => {
                 <div className="humanitas-prompt-div">
                     <p> Here is the prompt</p>
                 </div>
-                <div className="humanitas-input-container">
-                    <div className="humanitas-input-div">
-                        <input className="humanitas-input" type="text"/>
-                    </div>
-                </div>
+                <HumanitasInputForm sendResponse={handleSendResponse}/>
                 <Button variant="light" id="solvePuzzleButton" onClick={() => { dispatch(SetSolved(7));}}> click to solve this puzzle</Button>
                 
             
             </div>
         </div>
     )
+}
+
+class HumanitasInputForm extends React.Component { 
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputContent: "",
+        };
+        this.onHandleSubmit = this.onHandleSubmit.bind(this);
+        this.onHandleChange = this.onHandleChange.bind(this);
+    }
+
+    onHandleSubmit(e) {
+        e.preventDefault();
+        const response = this.state.inputContent;
+        this.props.sendResponse(response);
+        this.setState({
+            inputContent: ""
+        });
+    }
+
+    onHandleChange(e) {
+        this.setState({
+        inputContent: e.target.value
+        });
+    }
+
+
+    render() {
+        return ( 
+            <div className="humanitas-input-container">
+                <form onSubmit={this.onHandleSubmit}>
+                    <input type="submit" style={{ display: "none" }} />
+                    <div className="humanitas-input-div">
+                        <input className="humanitas-input" value={this.state.inputContent} onChange={this.onHandleChange} type="text"/>
+                    </div>
+                    <div className="enter-button-div">
+                        <Button type="submit" variant="light" id="submitPrompt" onClick={this.onHandleSubmit} >Submit</Button>
+                        
+                    </div>
+                </form>
+            </div>
+        )
+    }
+
 }
 
 export default Humanitas
