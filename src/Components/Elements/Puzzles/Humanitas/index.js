5c0f3716f6745a8dbde280ff9c4bb0d9bcdf834e
Adriana Cardenas <adricardenas7890@hotmail.com>
2021-04-03 00:30:32 -0500

    refactoring

--- src/Components/Elements/Puzzles/Humanitas/index.js
+++ src/Components/Elements/Puzzles/Humanitas/index.js
@@ -1,4 +1,4 @@
-import React, {setState} from 'react';
+import React from 'react';
 import { Button } from 'react-bootstrap';
 import { SetSolved } from '../puzzleProgressSlice';
 import { useDispatch } from 'react-redux';
