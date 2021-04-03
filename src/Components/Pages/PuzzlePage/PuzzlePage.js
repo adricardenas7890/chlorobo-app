7b0f2ea043ca4d7eae0d8a966a5eee6c5f501cea
Adriana Cardenas <adricardenas7890@hotmail.com>
2021-04-03 16:32:48 -0500

    junk changes

--- src/Components/Pages/PuzzlePage/PuzzlePage.js
+++ src/Components/Pages/PuzzlePage/PuzzlePage.js
@@ -7,7 +7,7 @@ import { connect } from 'react-redux';
 // import Caritas from '../../Elements/Puzzles/Caritas/index';
 // import Humanitas from '../../Elements/Puzzles/Humanitas/index';
 // import Industria from '../../Elements/Puzzles/Industria/index';
-import PuzzleCompletePage from '../PuzzleCompletePage/PuzzleCompletePage';
+
 import PuzzleContent from '../PuzzleContentPage/index';
 import PuzzlePoemContent from '../PuzzlePoemPage/index'
 
@@ -15,16 +15,63 @@ import PuzzlePoemContent from '../PuzzlePoemPage/index'
 // Connected to puzzleSlice reducer, will update when state.puzzle updates
 
 const PuzzlePage = ({ puzzle, poemMode }) => {
-    debugger;
     let puzzleComponent;
-    if (poemMode === 1) {
+    if (poemMode === true) {
+        //console.log("here it is");
         puzzleComponent = <PuzzlePoemContent puzzle={puzzle} />
+
+
+        // if (puzzle === "castitas") {
+        // puzzleComponent = <CastitasPoemContent/>
+        // }
+        // else if (puzzle === "temperantia") {
+        //     puzzleComponent = <TemperantiaPoemContent/>
+        // }
+        // else if (puzzle === "industria") {
+        //     puzzleComponent = <IndustriaPoemContent/>
+        // }
+        // else if (puzzle === "patientia") {
+        //     puzzleComponent = <PatientiaPoemContent/>
+        // }
+        // else if (puzzle === "humilitas") {
+        //     puzzleComponent = <HumilitasPoemContent/>
+        // }
+        // else if (puzzle === "caritas") {
+        //     puzzleComponent = <CaritasPoemContent/>
+        // }
+        // else if (puzzle === "humanitas") {
+        //     puzzleComponent = <HumanitasPoemContent/>
+        // }
+        // else {
+        //     puzzleComponent = <div>We didn't find a match!</div>
+        // }
     }
-    else if (poemMode === 2) {
+    else if (poemMode === false) {
         puzzleComponent = <PuzzleContent puzzle={puzzle}/>
-    }
-    else if (poemMode === 3) {
-        puzzleComponent = <PuzzleCompletePage puzzle={puzzle}/>
+        // if (puzzle === "castitas") {
+        //     puzzleComponent = <Castitas />
+        // }
+        // else if (puzzle === "temperantia") {
+        //     puzzleComponent = <Temperantia />
+        // }
+        // else if (puzzle === "industria") {
+        //     puzzleComponent = <Industria />
+        // }
+        // else if (puzzle === "patientia") {
+        //     puzzleComponent = <Patientia />
+        // }
+        // else if (puzzle === "humilitas") {
+        //     puzzleComponent = <Humilitas />
+        // }
+        // else if (puzzle === "caritas") {
+        //     puzzleComponent = <Caritas />
+        // }
+        // else if (puzzle === "humanitas") {
+        //     puzzleComponent = <Humanitas />
+        // }
+        // else {
+        //     puzzleComponent = <div>We didn't find a match!</div>
+        // }
     }
     return (
         <div>
