823694bae4a8715517e9b376bb870701af80be47
Benjamin Randall <bengr35@gmail.com>
2021-04-07 00:37:02 -0500

    style credits page (temporary)

--- src/Components/Elements/Credits/index.js
+++ src/Components/Elements/Credits/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import { Container, Row, Col  } from 'react-bootstrap';
 import './index.css';
 
 const Credits = () => {
     return (
-        <div>
-            <p>Credits page!</p>
+        <div id="main-credits-container">
+            <Container className="credits-text-temp">
+                <p class="credits-title font-heritage">Chlorobo</p>
+                <p>Thank you for playing Chlorobo.</p>
+                <p>This is a credits page that is under construction.</p>
+                <p>If you are beta testing this game, please remember to send your feedback!</p>
+            </Container>
+            <Container className="credits-text-rows">
+                <Row className="credits-row-justify">
+                    <p>Creative Director</p>
+                    <p className="credits-name">Benjamin Randall</p>
+                </Row>
+                <Row className="credits-row-justify">
+                    <p>Writer</p>
+                    <p className="credits-name">Celine M. Ndibwami</p>
+                </Row>
+                <Row className="credits-row-justify">
+                    <p>Composer</p>
+                    <p className="credits-name">Jonathan Randall</p>
+                </Row>
+                <Row className="credits-row-justify">
+                    <p>Software Developer</p>
+                    <p className="credits-name">Adriana Cardenas</p>
+                </Row>
+            </Container>
         </div>
     )
 }
