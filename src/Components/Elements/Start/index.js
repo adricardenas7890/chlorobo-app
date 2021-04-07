1d7135b7eed62c00f1d1ef773b9668d174c98648
Benjamin Randall <bengr35@gmail.com>
2021-04-07 14:55:58 -0500

    splash page adjustments

--- src/Components/Elements/Start/index.js
+++ src/Components/Elements/Start/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-import {mainMenuPage} from '../../Pages/Content/contentSlice';
+// import {mainMenuPage} from '../../Pages/Content/contentSlice';
 import {introPage} from '../../Pages/Content/contentSlice';
-import {creditsPage} from '../../Pages/Content/contentSlice';
+// import {creditsPage} from '../../Pages/Content/contentSlice';
+// import {endSequencePage} from '../../Pages/Content/contentSlice';
 import { useDispatch } from 'react-redux'
 import splashImage from './splash.png';
 import { isMobile } from 'react-device-detect';
@@ -19,6 +20,7 @@ const Start = () => {
                     </div>
                     <div>
                         <h1 className="mobile-text-1">This site is not accessible on mobile.</h1>
+                        <br/>
                         <h1 className="mobile-text-2">Please re-login from a desktop device.</h1>
                     </div>
                 </div>
@@ -27,12 +29,12 @@ const Start = () => {
             return (
                 <div>
                     <div>
-                        <img src={splashImage} className="App-logo" alt="Chlorobo-title" />
+                        <img src={splashImage} className="splash-logo" alt="Chlorobo-title" />
                     </div>
                     <br/>
                     <div>
                         <Button variant="light" onClick={() => dispatch(introPage())} >ENTER</Button>
-                        <Button variant="light" onClick={() => dispatch(creditsPage())} >CREDITS</Button>
+                        {/* <Button variant="light" onClick={() => dispatch(creditsPage())} >CREDITS</Button> */}
                     </div>
                 </div>
             ) 
