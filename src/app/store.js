6846bc11ac1a6849e15f9364da1f467b23ae46e4
Adriana Cardenas <adricardenas7890@hotmail.com>
2021-04-10 19:30:22 -0500

    working with serializable

--- src/app/store.js
+++ src/app/store.js
@@ -6,18 +6,35 @@ import temperantiaReducer from '../Components/Elements/Puzzles/Temperantia/tempe
 import { persistStore } from 'redux-persist';
 import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
+import storageSession from 'redux-persist/lib/storage/session';
+import {getDefaultMiddleware } from '@reduxjs/toolkit'
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
 
 // Which reducers will be persisted
 const persistConfig = { 
   key: 'root',
   storage,
-  whitelist: ['currentPuzzleProgress', 'currentTempProgress' ]
+  whitelist: ['currentPuzzleProgress', 'currentTempProgress']
 }
+
+const navigationConfig = {
+  key: 'navigation',
+  storage: storageSession,
+  whitelist: ['currentViewPage', 'currentPuzzle']
+}
+
 
 // abstract from configureStore
 const rootReducer = combineReducers({
-  currentViewPage: viewPageReducer,
-  currentPuzzle: puzzlePageReducer,
+  currentViewPage: persistReducer(navigationConfig,viewPageReducer),
+  currentPuzzle: persistReducer(navigationConfig, puzzlePageReducer),
   currentPuzzleProgress: puzzleProgressReducer,
   currentTempProgress: temperantiaReducer,
 });
@@ -27,7 +44,12 @@ const persistedReducers = persistReducer(persistConfig, rootReducer);
 
 
 export const store = configureStore({
-  reducer: persistedReducers
+  reducer: persistedReducers,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
 });
 export const persistor = persistStore(store);
 
