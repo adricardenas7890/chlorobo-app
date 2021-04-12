a26c64c0e9fb6861a97e14ff07398b6488af67f2
Benjamin Randall <bengr35@gmail.com>
2021-04-12 01:30:11 -0500

    remove lazy load

--- src/Components/Elements/Start/index.js
+++ src/Components/Elements/Start/index.js
@@ -7,7 +7,7 @@ import {helpPage} from '../../Pages/Content/contentSlice';
 import { useDispatch } from 'react-redux'
 import splashImage from './splash.png';
 import { isMobile } from 'react-device-detect';
-import LazyLoad from 'react-lazyload';
+// import LazyLoad from 'react-lazyload';
 // import { isChrome } from 'react-device-detect';
 import "./index.css"
 
@@ -15,24 +15,18 @@ const Start = () => {
         const dispatch = useDispatch();
         if (isMobile) {
             return (
-                <LazyLoad>
-                    <div className="mobile-splash-container"> 
-                        
-                        <img src={splashImage} className="mobile-splash" alt="Chlorobo-title" />
-                    
-                    <h1 className="mobile-text-1">The full site is not accessible on mobile.</h1>
-                    <br/>
-                    <h1 className="mobile-text-2"><Button id="acc-button-mobile-start" variant="light" onClick={() => dispatch(helpPage())} >SINGLE-PAGE VERSION</Button></h1>
-                    </div>
-                </LazyLoad>
+                <div className="mobile-splash-container"> 
+                    <img src={splashImage} className="mobile-splash" alt="Chlorobo-title" />
+                <h1 className="mobile-text-1">The full site is not accessible on mobile.</h1>
+                <br/>
+                <h1 className="mobile-text-2"><Button id="acc-button-mobile-start" variant="light" onClick={() => dispatch(helpPage())} >SINGLE-PAGE VERSION</Button></h1>
+                </div>
             )
         } else {
             return (
                 <div className="web-splash-container">
                     <div>
-                        <LazyLoad>
-                            <img src={splashImage} className="splash-logo" alt="Chlorobo-title" />
-                        </LazyLoad>
+                        <img src={splashImage} className="splash-logo" alt="Chlorobo-title" />
                     </div>
                     <div className="enter-button-div">
                         <Button id="enter-button-start" variant="light" onClick={() => dispatch(introPage())} >ENTER</Button>
